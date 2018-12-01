0f6df79c1365c87d4f627992f8dea93445f86063
Chatchai Saratakij <CSaratakij@gmail.com>
2018-12-02 01:15:17 +0700

    Make player able to walk.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,15 +2,96 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+namespace Ludumdare43
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField]
+        float walkSpeed;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        [SerializeField]
+        float runSpeed;
+
+        bool isToggleRun;
+        bool isTarget;
+
+        Vector2 inputVector;
+        Vector2 lastInputVector;
+
+        Vector3 velocity;
+        Vector3 lastInputDir;
+        Vector3 relativeVector;
+
+        Quaternion targetRotation;
+
+        Rigidbody rigid;
+        Status health;
+
+
+        void Awake()
+        {
+            Initialize();
+        }
+
+        void Update()
+        {
+            InputHandler();
+        }
+
+        void LateUpdate()
+        {
+            LookToInputDirection();
+        }
+
+        void FixedUpdate()
+        {
+            MovementHandler();
+        }
+
+        void Initialize()
+        {
+            rigid = GetComponent<Rigidbody>();
+        }
+
+        void InputHandler()
+        {
+            if (inputVector.x != 0.0f || inputVector.y != 0.0f)
+                lastInputVector = inputVector;
+
+            inputVector.x = Input.GetAxisRaw("Horizontal");
+            inputVector.y = Input.GetAxisRaw("Vertical");
+
+            if (inputVector.magnitude > 1.0f)
+                inputVector = inputVector.normalized;
+        }
+
+        void MovementHandler()
+        {
+            if (isToggleRun) {
+                velocity.x = (inputVector.x * runSpeed) * Time.fixedDeltaTime;
+                velocity.z = (inputVector.y * runSpeed) * Time.fixedDeltaTime;
+            }
+            else {
+                velocity.x = (inputVector.x * walkSpeed) * Time.fixedDeltaTime;
+                velocity.z = (inputVector.y * walkSpeed) * Time.fixedDeltaTime;
+            }
+
+            rigid.AddForce(velocity, ForceMode.Impulse);
+        }
+
+        void LookToInputDirection()
+        {
+            lastInputDir.x = lastInputVector.x;
+            lastInputDir.y = 0.0f;
+            lastInputDir.z = lastInputVector.y;
+            
+            relativeVector = (lastInputDir + transform.position) - transform.position;
+
+            if (relativeVector != Vector3.zero) {
+                targetRotation = Quaternion.LookRotation(relativeVector, Vector3.up);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 0.2f);
+            }
+        }
+    }
 }
